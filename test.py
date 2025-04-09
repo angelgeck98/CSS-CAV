d90aa89e46f5161287024bce228fd540551cac8f
devbones <thedevbones@gmail.com>
2025-04-09 16:24:45 -0500

    Added simulation used for Quanta Correspondent

--- test.py
+++ test.py
@@ -4,32 +4,58 @@ import time
 
 # Connect to the CARLA server
 client = carla.Client('localhost', 2000)
-client.set_timeout(10.0)
+client.set_timeout(60.0)
 world = client.get_world()
+traffic_manager = client.get_trafficmanager()
+tm_port = traffic_manager.get_port()
 
 # Load the car models
 blueprint_library = world.get_blueprint_library()
-vehicle1_bp = blueprint_library.find('vehicle.tesla.model3')
-vehicle2_bp = blueprint_library.find('vehicle.audi.a2')
+vehicles_bps = []
+
+# Filter for vehicle blueprints
+for bp in blueprint_library.filter('vehicle.*'):
+    vehicles_bps.append(bp)
+    
+print("Found {} vehicle blueprints.".format(len(vehicles_bps)))
+print("-------------------------------------------------")
 
 # Randomly choose two spawn points
 spawn_points = world.get_map().get_spawn_points()
-spawn_point1 = random.choice(spawn_points)
-spawn_point2 = carla.Transform(spawn_point1.location + carla.Location(x=-5, y=0, z=0), spawn_point1.rotation)
 
 # Spawn the cars and wait for 10 seconds
-vehicle1 = world.spawn_actor(vehicle1_bp, spawn_point1)
-vehicle2 = world.spawn_actor(vehicle2_bp, spawn_point2)
-time.sleep(10)
+for bp in vehicles_bps:
+    # Check if there are any spawn points left
+    if spawn_points == []:
+        break 
+    spawn_point = random.choice(spawn_points)
+    spawn_points.remove(spawn_point)
+    vehicle = world.spawn_actor(bp, spawn_point)
+    print("Spawned vehicle {} at {}".format(vehicle.id, spawn_point))
+    
+print("Spawned all vehicles. Waiting 3 seconds...")
+print("-------------------------------------------------")    
+time.sleep(3)
 
-# Start autopilot and control the cars for 30 seconds
-vehicle1.set_autopilot(True)
-vehicle1.apply_control(carla.VehicleControl(throttle=1.0, steer=0.0))
+# Start autopilot and control the cars
+for vehicle in world.get_actors().filter('vehicle.*'):
+    vehicle.set_autopilot(True)
+     # Randomly assign car as malicous
+    if random.random() > 1:
+        print(vehicle.id, "is NOT evil...")
+    else:
+        print(vehicle.id, "is evil!")
+        traffic_manager.ignore_lights_percentage(vehicle, 100.0)
+        traffic_manager.distance_to_leading_vehicle(vehicle, -20.0)
+        traffic_manager.vehicle_percentage_speed_difference(vehicle, -500.0)
+        traffic_manager.ignore_vehicles_percentage(vehicle, 100.0)
 
-vehicle2.set_autopilot(True)
-vehicle2.apply_control(carla.VehicleControl(throttle=1.0, steer=0.0))
-time.sleep(30)
+print("Running vehicles in autopilot for 120 seconds...")
+print("-------------------------------------------------")
+time.sleep(120)
 
 # Destroy the cars
-vehicle1.destroy()
-vehicle2.destroy()
+for vehicle in world.get_actors().filter('vehicle.*'):
+    vehicle.destroy()
+    
+print("Destroyed all vehicles. Goodbye!")
